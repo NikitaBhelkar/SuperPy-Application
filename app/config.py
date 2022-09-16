2954dc2fe887e8ad839bb156456358fe96cd9211
NikitaBhelkar <102810045+NikitaBhelkar@users.noreply.github.com>
2022-09-16 19:08:03 +0200

    Delete app directory

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os,os.path
-
-ROOT_DIR = os.path.abspath(os.curdir)
-bought_file = ROOT_DIR+"/data/bought.csv"
-sold_file = ROOT_DIR+"/data/sold.csv"
-bought_file_copy = ROOT_DIR+"/data/bought_file_copy.csv"
-artificial_date = ROOT_DIR+"/data/artificial_date.txt"
-products_expired = ROOT_DIR+"/data/products_expired_before_sell.csv"
